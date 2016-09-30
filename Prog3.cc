601d7fc95f1cace853f6494f78a18df2670f2931
Krishan Madan <krm219@miranda.cse.lehigh.edu>
2016-09-29 21:08:59 -0400

    Pro

---
+++ Prog3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+
+
+#include "Trie.h"
+
+int main(int argc, char* argv[])
+{
+  Trie t;
+   t.put("LEHIGH", 1);
+  // t.put("LEMON", 1);
+  t.get("LEHIGH");
+  // t.get("LEMON");
+
+
+}
